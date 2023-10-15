f8eb17482bf736ee0e6ab7076584802e53d50d22
bt-nghia <bnghia7303@gmail.com>
2023-10-15 16:38:40 +0700

    try nw ~ [0.1, 0.9], sw = 1

--- run.sh
+++ run.sh
@@ -8,12 +8,23 @@
 # python train.py -w1 0.8 -w2 0.2
 # python train.py -w1 0.9 -w2 0.1
 
-python train.py -sw 0.9 -nw 0.1
-python train.py -sw 0.8 -nw 0.2
-python train.py -sw 0.7 -nw 0.3
-python train.py -sw 0.6 -nw 0.4
-python train.py -sw 0.5 -nw 0.5
-python train.py -sw 0.4 -nw 0.6
-python train.py -sw 0.3 -nw 0.7
-python train.py -sw 0.2 -nw 0.8
-python train.py -sw 0.1 -nw 0.9
+# python train.py -sw 0.9 -nw 0.1
+# python train.py -sw 0.8 -nw 0.2
+# python train.py -sw 0.7 -nw 0.3
+# python train.py -sw 0.6 -nw 0.4
+# python train.py -sw 0.5 -nw 0.5
+# python train.py -sw 0.4 -nw 0.6
+# python train.py -sw 0.3 -nw 0.7
+# python train.py -sw 0.2 -nw 0.8
+# python train.py -sw 0.1 -nw 0.9
+
+# try this instead
+python train.py -sw 1 -nw 0.9
+python train.py -sw 1 -nw 0.8
+python train.py -sw 1 -nw 0.7
+python train.py -sw 1 -nw 0.6
+python train.py -sw 1 -nw 0.5
+python train.py -sw 1 -nw 0.4
+python train.py -sw 1 -nw 0.3
+python train.py -sw 1 -nw 0.2
+python train.py -sw 1 -nw 0.1
