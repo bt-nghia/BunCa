59b31fda9ff7d843614483fac3d3a572c8d74d72
bt-nghia <bnghia7303@gmail.com>
2023-11-06 15:47:29 +0700

    fix sh file

--- run.sh
+++ run.sh
@@ -29,10 +29,20 @@
 # python train.py -sw 1 -nw 0.2
 # python train.py -sw 1 -nw 0.1
 
+# Youshu
+python train.py
+
+# clothing
 python gene_ii_co_oc.py -d clothing
 python preProcess.py -d clothing
-python train.py -d clothing
+python train.py -d clothing -w3 0.8 -w4 0.2
 
+# food
 python gene_ii_co_oc.py -d food
 python preProcess.py -d food
 python train.py -d food -w3 0.8 -w4 0.2
+
+# elctronic
+python gene_ii_co_oc.py -d electronic
+python preProcess.py -d electronic
+python train.py -d electronic -w3 0.8 -w4 0.2
