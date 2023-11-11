7be0fbf2631fb586f162962d47a335b6f01fbf5f
bt-nghia <bnghia7303@gmail.com>
2023-11-11 09:34:10 +0700

    fix all data

--- gene_ii_co_oc.py
+++ gene_ii_co_oc.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
     paras = get_cmd().__dict__
     dataset_name = paras["dataset"]
 
-    if dataset_name in ['Youshu', 'iFashion', 'Netease', 'food1', 'electronic1', 'clothing1']:
+    if dataset_name in ['Youshu', 'iFashion', 'Netease', 'food1', 'electronic1', 'clothing1', 'clothing', 'electronic', 'food']:
         sep = '\t'
         file_type = '.txt'
     else:
