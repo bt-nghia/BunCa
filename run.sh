b6869acd2d2484ec3f990d9cb1ceb2423b4f4a93
bt-nghia <bnghia7303@gmail.com>
2023-11-22 22:39:12 +0700

    finish run.sh for amazon

--- run.sh
+++ run.sh
@@ -17,14 +17,14 @@ python train.py -d NetEase -w3 0.8 -w4 0.8
 # clothing
 python gene_ii_co_oc.py -d clothing
 python preProcess.py -d clothing
-!python train.py -d clothing -w3 0.1 -w4 0.1 -sw 0.8 -nw 0.2
+python train.py -d clothing -w3 0.1 -w4 0.1 -sw 0.8 -nw 0.2
 
 # food
 python gene_ii_co_oc.py -d food
 python preProcess.py -d food
-python train.py -d food -w3 0.9 -w4 0.1
+python train.py -d food -w3 0.1 -w4 0.1 -sw 0.8 -nw 0.2
 
 # elctronic
 python gene_ii_co_oc.py -d electronic
 python preProcess.py -d electronic
-python train.py -d electronic -w3 0.9 -w4 0.1
+python train.py -d electronic -w3 0.1 -w4 0.1 -sw 0.8 -nw 0.2
