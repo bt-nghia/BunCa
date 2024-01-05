c0e8f8aa19f289e1db75d366ac3c7fa5035a34e2
bt-nghia <bnghia7303@gmail.com>
2024-01-05 11:52:09 +0700

    abl_view

--- run.sh
+++ run.sh
@@ -1,23 +1,19 @@
 # iFashion
-# python gene_ii_co_oc.py -d iFashion
-# python preProcess.py -d iFashion -iui 15 -ibi 2
-# python train.py -d iFashion -w1 0.4 -w2 0.4 -w3 1 -w4 1 -sw 0.9 -nw 0.1
-# python train.py -d iFashion -w1 0.6 -w2 0.6 -w3 1 -w4 1 -sw 0.9 -nw 0.1
+python gene_ii_co_oc.py -d iFashion
+python preProcess.py -d iFashion -iui 15 -ibi 2
+python train.py -d iFashion -w1 0.6 -w2 0.6 -w3 1 -w4 1 -sw 0.9 -nw 0.1
 
 
 # Youshu
-# python gene_ii_co_oc.py -d Youshu
-# python preProcess.py -d Youshu -ibi 4 -iui 4
-# python train.py -d Youshu -w1 1 -w2 1 -w3 1 -w4 1 -sw 0 -nw 1
+python gene_ii_co_oc.py -d Youshu
+python preProcess.py -d Youshu -ibi 4 -iui 4
+python train.py -d Youshu -w1 1 -w2 1 -w3 1 -w4 1 -sw 0 -nw 1
+
 
 # NetEase
 python gene_ii_co_oc.py -d NetEase
 python preProcess.py -d NetEase -ibi 10 -iui 9
-# python train.py -d NetEase -w1 0.8 -w2 0.8 -w3 0.8 -w4 0.8
-python train.py -d NetEase -w1 1.2 -w2 1.2 -w3 0.8 -w4 0.8
-# python train.py -d NetEase -w1 0.6 -w2 0.6 -w3 0.8 -w4 0.8
-
-# python train.py -d NetEase -w3 0 -w4 0
+python train.py -d NetEase -w3 0.8 -w4 0.8
 
 
 # # clothing
@@ -25,10 +21,12 @@ python train.py -d NetEase -w1 1.2 -w2 1.2 -w3 0.8 -w4 0.8
 # python preProcess.py -d clothing
 # python train.py -d clothing -w3 0.1 -w4 0.1 -sw 0.8 -nw 0.2
 
+
 # # food
 # python gene_ii_co_oc.py -d food
 # python preProcess.py -d food -ibi 2 -iui 4
 # python train.py -d food -w3 0.1 -w4 0.1 -sw 0.9 -nw 0.8
+
 
 # # elctronic
 # python gene_ii_co_oc.py -d electronic
