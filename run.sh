41899da4f365737631d53d1c49f7141fd3aafe90
bt-nghia <bnghia7303@gmail.com>
2024-01-12 19:42:13 +0700

    update all

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # iFashion
 python gene_ii_co_oc.py -d iFashion
 python preProcess.py -d iFashion -iui 15 -ibi 2
-python train.py -d iFashion -w1 0.6 -w2 0.6 -w3 1 -w4 1 -sw 0.9 -nw 0.1
+python train.py -d iFashion -w1 0.6 -w2 0.6 -w3 0.9 -w4 0.9 -sw 0.9 -nw 0.1
 
 
 # Youshu
