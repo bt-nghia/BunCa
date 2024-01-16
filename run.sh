8285133651de76f450d567d6e3bbf06a7bd1588f
bt-nghia <bnghia7303@gmail.com>
2024-01-16 10:43:30 +0700

    final

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 # iFashion
-# python gene_ii_co_oc.py -d iFashion
-# python preProcess.py -d iFashion -iui 15 -ibi 2
-# python train.py -d iFashion -w1 0.6 -w2 0.6 -w3 0.9 -w4 0.9 -sw 0.9 -nw 0.1
+python gene_ii_co_oc.py -d iFashion
+python preProcess.py -d iFashion -iui 15 -ibi 2
+python train.py -d iFashion -w1 0.6 -w2 0.6 -w3 0.9 -w4 0.9 -sw 0.9 -nw 0.1
 
 
 # Youshu
-# python gene_ii_co_oc.py -d Youshu
-# python preProcess.py -d Youshu -ibi 4 -iui 4
-# python train.py -d Youshu -w1 1 -w2 1 -w3 0.9 -w4 0.9 -sw 0 -nw 1
+python gene_ii_co_oc.py -d Youshu
+python preProcess.py -d Youshu -ibi 4 -iui 4
+python train.py -d Youshu -w1 1 -w2 1 -w3 0.9 -w4 0.9 -sw 0 -nw 1
 
 
 # NetEase
