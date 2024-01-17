fd7e0d6f7e024475a4acbd63305a5d470fe835e2
bt-nghia <bnghia7303@gmail.com>
2024-01-17 17:06:16 +0700

    try NetEase 21

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 # iFashion
-python gene_ii_co_oc.py -d iFashion
-python preProcess.py -d iFashion -iui 15 -ibi 2
+# python gene_ii_co_oc.py -d iFashion
+# python preProcess.py -d iFashion -iui 15 -ibi 2
 # python train.py -d iFashion -w1 0.6 -w2 0.6 -w3 0.9 -w4 0.9 -sw 0.9 -nw 0.1
-python train.py -d iFashion -w1 0.6 -w2 0.6 -w3 0.9 -w4 0.9 -sw 0 -nw 1
 
 
 
@@ -13,9 +12,9 @@ python train.py -d iFashion -w1 0.6 -w2 0.6 -w3 0.9 -w4 0.9 -sw 0 -nw 1
 
 
 # # NetEase
-# python gene_ii_co_oc.py -d NetEase
-# python preProcess.py -d NetEase -ibi 10 -iui 9
-# python train.py -d NetEase -w1 1 -w2 1 -w3 0.8 -w4 0.8 -sw 0 -nw 1
+python gene_ii_co_oc.py -d NetEase
+python preProcess.py -d NetEase -ibi 10 -iui 9
+python train.py -d NetEase -w1 1 -w2 1 -w3 0.8 -w4 0.8 -sw 0 -nw 1
 
 
 # # clothing
