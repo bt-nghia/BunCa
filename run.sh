74ca62f08979226fa94e3793b14a6536f3362368
bt-nghia <bnghia7303@gmail.com>
2024-01-17 14:06:19 +0700

    try iFashion

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 # iFashion
 python gene_ii_co_oc.py -d iFashion
 python preProcess.py -d iFashion -iui 15 -ibi 2
-python train.py -d iFashion -w1 0.6 -w2 0.6 -w3 0.9 -w4 0.9 -sw 0.9 -nw 0.1
+# python train.py -d iFashion -w1 0.6 -w2 0.6 -w3 0.9 -w4 0.9 -sw 0.9 -nw 0.1
+python train.py -d iFashion -w1 0.6 -w2 0.6 -w3 0.9 -w4 0.9 -sw 0 -nw 1
+
 
 
 # Youshu
